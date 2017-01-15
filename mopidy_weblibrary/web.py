b864107c6ebd2155f5e8e382769335fe9f59d794
Federico Barresi <fede.barresi@gmail.com>
2017-01-15 23:19:26 +0100

    adjust line sizes

--- mopidy_weblibrary/web.py
+++ mopidy_weblibrary/web.py
@@ -54,7 +54,9 @@ def get_javascript_templates():
                 </td>
                 <td>
                     <p class="size">Processing...</p>
-                    <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
+                    <div class="progress progress-striped active" role="progressbar" aria-valuemin="0"
+                    aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success"
+                    style="width:0%;"></div></div>
                 </td>
                 <td>
                     {% if (!i && !o.options.autoUpload) { %}
@@ -80,14 +82,16 @@ def get_javascript_templates():
                 <td>
                     <span class="preview">
                         {% if (file.thumbnailUrl) { %}
-                            <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
+                            <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery>
+                            <img src="{%=file.thumbnailUrl%}"></a>
                         {% } %}
                     </span>
                 </td>
                 <td>
                     <p class="name">
                         {% if (file.url) { %}
-                            <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
+                            <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}"
+                            {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
                         {% } else { %}
                             <span>{%=file.name%}</span>
                         {% } %}
@@ -101,7 +105,9 @@ def get_javascript_templates():
                 </td>
                 <td>
                     {% if (file.deleteUrl) { %}
-                        <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
+                        <button class="btn btn-danger delete" data-type="{%=file.deleteType%}"
+                        data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %}
+                        data-xhr-fields='{"withCredentials":true}'{% } %}>
                             <i class="glyphicon glyphicon-trash"></i>
                             <span>Delete</span>
                         </button>
